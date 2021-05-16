11773dc8d1f6f4db07ba37d10842c12dd07786e5
Al_Ros <kolliak.tbl@gmail.com>
2021-05-16 21:15:05 +0300

    y answer to second task

--- js/FirstProject.js
+++ js/FirstProject.js
@@ -19,7 +19,7 @@
 // personalMovieDB.movies =[prompt('Last movie you watched?'] = prompt('Rate this movie');
 
 
-const numberOfFilms = +prompt('How many movies have you watched so far?');
+let numberOfFilms = +prompt('How many movies have you watched so far?', '');
 
 const personalMovieDB =  {
     count: numberOfFilms,
@@ -29,12 +29,32 @@ const personalMovieDB =  {
     privat: false
 };
 
-const a = prompt('Last movie you watched?', ''),
-      b = prompt('Rate this movie', ''),
-      c = prompt('Last movie you watched?', ''),
-      d = prompt('Rate this movie', '');
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+// let i = 0;
+// let a = 0, b = 0;
+// if (numberOfFilms > 0) {
+//     do {
+//         do {
+//             a = prompt('Last movie you watched?', '');
+//         }
+//         while (a.length === 0 || a.length > 50);
+        
+//         b = prompt('Rate this movie', '');
+//             personalMovieDB.movies[a] = b;
+//         i++;
+//     }
+//     while ( i < numberOfFilms);
+// } else {
+//     console.log("Realy?! No one?!");
+// }
+
+if (numberOfFilms > 0 && numberOfFilms < 10) {
+    console.log('You watched little movies');
+}else if(numberOfFilms >= 10 && numberOfFilms < 30) {
+    console.log('You are average wacher!');
+} else {
+    console.log('You are movie maniac!');
+}
 
 console.log(personalMovieDB);
+
