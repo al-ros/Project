5981f4697fc53ea042fdce78cf82e4bb2e5f4ef3
Al_Ros <kolliak.tbl@gmail.com>
2021-05-17 20:22:58 +0300

    Teacher's answer

--- js/FirstProject.js
+++ js/FirstProject.js
@@ -18,6 +18,7 @@
 
 // personalMovieDB.movies =[prompt('Last movie you watched?'] = prompt('Rate this movie');
 
+// My answer:
 
 let numberOfFilms = +prompt('How many movies have you watched so far?', '');
 
@@ -48,13 +49,46 @@ const personalMovieDB =  {
 //     console.log("Realy?! No one?!");
 // }
 
-if (numberOfFilms > 0 && numberOfFilms < 10) {
+if (personalMovieDB.count < 10) {
     console.log('You watched little movies');
-}else if(numberOfFilms >= 10 && numberOfFilms < 30) {
+}else if(personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
     console.log('You are average wacher!');
-} else {
+} else if (personalMovieDB.count >= 30) {
     console.log('You are movie maniac!');
+} else {
+    console.log('Error!');
 }
 
-console.log(personalMovieDB);
+// console.log(personalMovieDB);
 
+
+
+
+
+// Teachers answer:
+
+// let numberOfFilms = +prompt('How many movies have you watched so far?', '');
+
+// const personalMovieDB =  {
+//     count: numberOfFilms,
+//     movies:  {},
+//     actors: {},
+//     genres: [],
+//     privat: false
+// };
+ 
+// for (let i = 0; i < 2; i++) {
+//     const a = prompt('Last movie you watched?', ''),
+//           b = prompt('Rate this movie', '');
+
+//     if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+//         personalMovieDB.movies[a] = b;
+//         console.log('Done');
+//     } else {
+//         console.log('Error');
+//         i--;
+//     }
+    
+// }
+
+// console.log(personalMovieDB);
