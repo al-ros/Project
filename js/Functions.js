851baa7a42e01bc8b06e02127e4bedd996522d51
Al_Ros <kolliak.tbl@gmail.com>
2021-05-25 19:36:50 +0300

    methods and properties

---
+++ js/Functions.js
@@ -0,0 +1,72 @@
+"use strict";
+
+//Function Declaration:
+
+// let num = 20;
+
+
+// showFirstMessage('Hello world!');
+
+
+// function showFirstMessage(text) {
+//     console.log(text);
+//     // let num = 10;
+//     console.log(num);
+// }
+
+// showFirstMessage('Hello world!');
+// console.log(num);
+
+
+
+
+// function calc(a, b) {
+//     return (a + b);
+//     // console.log('abcd');
+// }
+
+// console.log(calc(4, 3));
+// console.log(calc(5, 6));
+// console.log(calc(10, 6));
+
+
+
+// function ret() {
+//     let num = 50;
+
+//     // Some big calculation
+
+
+//     return num;
+// }
+
+// const anotherNum = ret();
+// console.log(anotherNum);
+
+
+
+
+
+// Function Expression:
+
+// logger();
+
+// const logger = function() {
+//     console.log('Hello');
+// };
+
+// logger();
+
+
+
+
+// Strelochnaya function:
+
+const someFunction = a => a + 4;
+
+const calc = (a, b) => a + b;
+
+const bigFunction = (a, b) => {
+    console.log('1');
+    return a + b;
+};
